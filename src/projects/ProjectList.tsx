25752d18df984a1b848bf1beee700845c399395c
Deniz Saganak <saganak.deniz@gmail.com>
2023-01-04 12:56:38 +0100

    edit & save & make error handling

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -8,9 +8,10 @@ import ProjectForm from "./ProjectForm";
 
 interface ProjectListProps {
     projects: Project[];
+    onSave: (project: Project) => void;
 }
 
-export default function ProjectList({ projects }: ProjectListProps) {
+export default function ProjectList({ projects, onSave }: ProjectListProps) {
     const [projectBeingEdited, setProjectBeingEdited] = useState({});
     
     const handleEdit = (project: Project) => {
@@ -18,12 +19,20 @@ export default function ProjectList({ projects }: ProjectListProps) {
         setProjectBeingEdited(project)
     }
 
+    //In a parent component, implement a function and pass it as a prop to a child component:
+    const cancelEditing = () => {
+        setProjectBeingEdited({});
+    }
+
     return (
     <div className="row">
        {projects.map((project) => (
         <div key={project.id} className="cols-sm">
            {project === projectBeingEdited ? 
-            <ProjectForm /> :
+            <ProjectForm
+              project={project}
+              onSave={onSave}
+              onCancel={cancelEditing} /> :
             <ProjectCard 
               project={project}
               onEdit={handleEdit} />
--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,11 +1,34 @@
+import { useState } from "react";
 import { MOCK_PROJECTS } from "./MockProjects";
 import ProjectList from "./ProjectList";
+import { Project } from "./Project";
 
 export default function ProjectsPage() {
+  const [projects, setProjects] = useState<Project[]>(MOCK_PROJECTS);
+
+  const saveProject = (project: Project) => {
+      //console.log('Saving project', project);
+    let updatedProjects = projects.map((p: Project) => {
+      return p.id === project.id ? project : p;
+    });
+    setProjects(updatedProjects);
+  }
+
   return (
     <div>
     <h1>Projects</h1>
-    <><ProjectList projects={MOCK_PROJECTS} /></>
+    <ProjectList 
+        onSave={saveProject}
+        projects={projects} />
     </div>
   )
 }
+
+
+
+
+
+
+/* 1. saveProject & 2. onSave={saveProject}
+1. Add a saveProject event handler that takes a project to ProjectPage and console.log's the project out.
+2. Wire up the onSave event of the <ProjectList /> component rendered in the ProjectPage to the saveProject event handler. */
